9ea3bf98a1fdcd4a1a53de87a632a64930b7f587
Yuvraj Pandey <email@gmail.com>
2025-06-16 16:55:10 +0000

    Completed the chat endpoint completely

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -98,10 +98,22 @@ app.post("/room", middleware, async (req, res) => {
 
 });
 
+app.get("/chats/:roomId", async (req,res) => {
+  const roomId = Number(req.params.roomId);
+  const messages = await prismaClient.chat.findMany({
+    where: {
+      roomId: roomId
+    },
+    orderBy: {
+      id: "desc"
+    },
+    take: 50
+  });
+    res.json({
+      messages
+    })
 
-
-
-
+})
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,29 +1,95 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
-
-
+import { parse } from "dotenv";
+import {prismaClient} from "@repo/db/client"
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws,request) => {
+
+interface User {
+    ws: WebSocket,
+    rooms: string[],
+    userId: string
+}
+
+const users: User[] = [];
+
+function checkUser(token: string): string | null {
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (typeof decoded == "string") {
+            return null;
+        }
+
+        if (!decoded || !decoded.userId) {
+            return null
+        }
+
+        return decoded.userId;
+    } catch (e) {
+        return null;
+    }
+    return null
+}
+
+wss.on("connection", (ws, request) => {
     const url = request.url;
-    if(!url) {
+    if (!url) {
         return;
     }
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token = queryParams.get('token') ?? "";
-    const decoded = jwt.verify(token,JWT_SECRET);
+    const userId = checkUser(token);
 
-
-
-    if(!decoded || !(decoded as JwtPayload).userId) {
-        ws.close();
-        return;
+    if (userId == null) {
+        ws.close()
+        return null;
     }
 
-    ws.on("message", (message) => {
-        ws.send("Hello from server"); 
-    });
-});
+    users.push({
+        userId,
+        rooms: [],
+        ws
+    })
 
-console.log("WebSocket server is running on port 8080");
+    ws.on('message',async function message(data) {
+        const parsedData = JSON.parse(data as unknown as string);
+
+        if (parsedData.type === "join_room") {
+            const user = users.find(x => x.ws === ws);
+            user?.rooms.push(parsedData.roomId);
+        }
+        if (parsedData.type === "leave_room") {
+            const user = users.find(x => x.ws === ws);
+            if (!user) {
+                return;
+            }
+            user.rooms = user?.rooms.filter(x => x === parsedData.room)
+        }
+
+        if (parsedData.type === "chat") {
+            const roomId = parsedData.roomId;
+            const message = parsedData.message;
+// use ques instead of directly saving the chats in database , what if database call fails ?
+
+            await prismaClient.chat.create({
+                data: {
+                    roomId,
+                    message,
+                    userId
+                }
+            })
+
+            users.forEach(user => {
+                if (user.rooms.includes(roomId)) {
+                    user.ws.send(JSON.stringify({
+                        type: "chat",
+                        message: message,
+                        roomId
+                    }))
+                }
+            })
+        }
+    })
+});
